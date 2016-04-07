01811a2fff7b2ca798f5f0464f00f8604d62e80e
Aurele-Barriere <aurele.barriere@gmail.com>
2016-04-07 11:09:27 +0200

    toplevel.cc handles toplevel

--- Code/main.cc
+++ Code/main.cc
@@ -2,36 +2,8 @@
 #include "object.hh"
 #include "env.hh"
 #include "eval.hh"
-
-extern Object just_read;
-extern "C" int yyparse();
-extern "C" FILE *yyin;
-
-using namespace std;
+#include "toplevel.hh"
 
 int main() {
-
-  Environment env;
-
-  do {
-    cout << "Lisp? " << flush;
-    yyparse();
-    Object l = just_read;
-    if (listp(l) && Object_to_string(car(l)) == "setq") {
-      env.add_new_binding(Object_to_string(cadr(l)), caddr(l));
-    }
-    else {
-      try {
-	cout << eval(l, env) << endl;
-      } catch (const Evaluation_Exception except) {
-	cout << except.what() << endl;
-      } catch (const No_Binding_Exception except) {
-	cout << except.what() << endl;
-      } catch (const Zipping_Exception except) {
-	cout << except.what() << endl;
-      } catch (const Bad_Type_Exception except) {
-	cout << except.what() << endl;
-      }
-    }
-  } while (!feof(yyin));
+  toplevel();
 }
---
+++ Code/toplevel.hh
@@ -0,0 +1,3 @@
+void prompt();
+void toplevel();
+
