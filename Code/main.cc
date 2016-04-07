4d623ddf02df0e8295106d20929bd0f05d2f05f9
Aurele-Barriere <aurele.barriere@gmail.com>
2016-04-07 10:57:21 +0200

    fixing setq, removing old unused objects

--- Code/main.cc
+++ Code/main.cc
@@ -10,20 +10,14 @@ extern "C" FILE *yyin;
 using namespace std;
 
 int main() {
-  Object a =  symbol_to_Object("a");
-  Object b =  symbol_to_Object("b");
-  Object one = number_to_Object(1);
-  Object two = number_to_Object(2);
 
   Environment env;
-  env.add_new_binding(Object_to_string(a), one);
-  env.add_new_binding(Object_to_string(a), two);
 
   do {
     cout << "Lisp? " << flush;
     yyparse();
     Object l = just_read;
-    if (Object_to_string(car(l)) == "setq") {
+    if (listp(l) && Object_to_string(car(l)) == "setq") {
       env.add_new_binding(Object_to_string(cadr(l)), caddr(l));
     }
     else {
