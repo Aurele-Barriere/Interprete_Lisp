714958895f47012c0f0671579c774310dfff7799
Jérémy Thibault <jeremy.thibault@ens-rennes.fr>
2016-04-08 14:01:09 +0200

    bla

--- Code/toplevel.cc
+++ Code/toplevel.cc
@@ -20,14 +20,14 @@ void toplevel() {
   Environment env; //creating environment
   int eof = 0; //boolean for end of file, modified by read
 
-  
+
   do {
     prompt();
-    
+
     Object l = read(&eof);
-    
+
     if (listp(l) && Object_to_string(car(l)) == "setq") {
-      env.add_new_binding(Object_to_string(cadr(l)), caddr(l));
+      env.add_new_binding(Object_to_string(cadr(l)), eval(caddr(l), env));
     }
     else {
       try {
