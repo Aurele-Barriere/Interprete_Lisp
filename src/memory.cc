a9979e486d55a2ed5fe8525d3b6622bd87466470
Aurèle <aurele.barriere@gmail.com>
2016-04-14 22:34:12 +0200

    cell_vect and flags vectors

--- src/memory.cc
+++ src/memory.cc
@@ -1,4 +1,16 @@
 #include "memory.hh"
+#include <iostream>
+
+using namespace std;
+
+Memory::Memory() {
+  cout << "Creating Memory" << endl;
+  cout << "Mem size : " << MEMORY_SIZE << endl;
+  int i;
+  for (i = 0; i< MEMORY_SIZE; i++) {
+    flags[i] = free;
+  }
+}
 
 void Memory::allocate_cell() {
 }
--- src/memory.hh
+++ src/memory.hh
@@ -1,12 +1,21 @@
 #pragma once
 
+#include "cell.hh"
 #include "object.hh"
+#include <vector>
 
 using namespace std;
 
+#define MEMORY_SIZE 4096
+
+enum flag { free , taken };
+
+
 class Memory {
 private:
-  
+Cell cell_vect[MEMORY_SIZE];
+flag flags[MEMORY_SIZE];
 public:
+Memory();
   void allocate_cell();
 };
