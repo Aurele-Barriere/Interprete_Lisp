889a37ad940e9c8b0c2c6e327f12a56ec50df4be
Aurèle <aurele.barriere@gmail.com>
2016-04-22 18:35:12 +0200

    made allocate function private

--- src/memory.hh
+++ src/memory.hh
@@ -2,6 +2,7 @@
 
 #include "cell.hh"
 #include "env.hh"
+#include "object.hh"
 
 #define BASE_MEMORY_SIZE 30
 
@@ -9,16 +10,23 @@ enum flag { not_taken , taken };
 
 
 class Memory {
+
+  friend Object cons(Object a, Object l);
+  friend Object number_to_Object(int n);
+  friend Object string_to_Object(std::string s);
+  friend Object symbol_to_Object(std::string s);
+
 private:
   Cell* cell_vect;
   flag* flags;
   unsigned size;
   void garbage_collection_aux(bool* seen, Object p);
+  Object allocate_cell();
 
 public:
   Memory();
   ~Memory();
-  Object allocate_cell();
+  
   Cell* at(unsigned i);
   void garbage_collection(Environment env);
 };
